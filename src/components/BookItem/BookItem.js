515b61f4d76a184c630aae02d443422d7a363ff6
Marko-batman <krznarm26@gmail.com>
2022-02-25 23:47:31 +0100

    Added form and edit book functionality

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -1,21 +1,63 @@
 import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 import { BooksContext } from "../../data/BooksContext";
 
 export default function BookItem({ book }) {
   const [books, setBooks] = useContext(BooksContext);
+  const [editName, setEditName] = useState("");
+  const [editAuthor, setEditAuthor] = useState("");
 
-  const deleteBook = (e) => {
+  const deleteBook = () => {
     const existBook = books.find((x) => x.name === book.name);
     if (existBook) {
       setBooks(books.filter((x) => x.name !== book.name));
     }
   };
 
+  const handleEditName = (e) => {
+    setEditName(e.target.value);
+  };
+
+  const handleEditAuthor = (e) => {
+    setEditAuthor(e.target.value);
+  };
+
+  const handleEditBook = (e) => {
+    e.preventDefault();
+    const bookId = book.id;
+    setBooks(
+      books.map((book) =>
+        book.id === bookId
+          ? { name: editName, author: editAuthor, id: bookId }
+          : book
+      )
+    );
+    setEditName("");
+    setEditAuthor("");
+  };
+
   return (
     <div className="block--book-item">
       <p>{book.name}</p>
       <p>{book.author}</p>
-      <button onClick={deleteBook}>Delete</button>
+      <form onSubmit={handleEditBook}>
+        <input
+          type="text"
+          name="name"
+          value={editName}
+          onChange={handleEditName}
+          placeholder="Naziv knjige"
+        />
+        <input
+          type="text"
+          name="author"
+          value={editAuthor}
+          onChange={handleEditAuthor}
+          placeholder="Ime i prezime autora"
+        />
+        <button type="submit">Ažuriraj</button>
+      </form>
+      <button onClick={deleteBook}>Obriši</button>
     </div>
   );
 }
