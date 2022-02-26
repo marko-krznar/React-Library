8acb7cb069d607cf3549ae2f82c868b2b2fb68f1
Marko-batman <krznarm26@gmail.com>
2022-02-26 20:57:25 +0100

    Added component for single book, created new route and link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import BooksPage from "./containers/BooksPage/BooksPage";
 import Header from "./containers/Header/Header";
 import Homepage from "./containers/Homepage/Homepage";
+import SingleBook from "./containers/SingleBook/SingleBook";
 import { BooksProvider } from "./data/BooksContext";
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/knjige" element={<BooksPage />} />
+          <Route path="/knjiga/:id" element={<SingleBook />} />
           <Route path="/*" element={<BooksPage />} />
         </Routes>
       </BooksProvider>
--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -1,37 +1,17 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { BooksContext } from "../../data/BooksContext";
 import "./style.scss";
 
 export default function BookItem({ book }) {
-  const {
-    editName,
-    editAuthor,
-    deleteBook,
-    handleEditName,
-    handleEditAuthor,
-    handleEditBook,
-  } = useContext(BooksContext);
+  const { deleteBook } = useContext(BooksContext);
 
   return (
     <tr className="block--book-item">
       <td>{book.name}</td>
       <td>{book.author}</td>
       <td>
-        <input
-          type="text"
-          name="name"
-          value={editName}
-          onChange={handleEditName}
-          placeholder="Naziv knjige"
-        />
-        <input
-          type="text"
-          name="author"
-          value={editAuthor}
-          onChange={handleEditAuthor}
-          placeholder="Ime i prezime autora"
-        />
-        <button onClick={() => handleEditBook(book)}>Ažuriraj</button>
+        <Link to={`/knjiga/${book.id}`}>Ažuriraj</Link>
       </td>
       <td>
         <button onClick={() => deleteBook(book)}>Obriši</button>
