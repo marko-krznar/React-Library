f88fb11fbe225606509b5595596235df7021b99f
Marko-batman <krznarm26@gmail.com>
2022-02-26 19:30:31 +0100

    Moved all functions to BookContext

--- src/data/BooksContext.js
+++ src/data/BooksContext.js
@@ -20,8 +20,73 @@ export const BooksProvider = (props) => {
       id: 3,
     },
   ]);
+
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [editName, setEditName] = useState("");
+  const [editAuthor, setEditAuthor] = useState("");
+
+  const newName = (e) => {
+    setName(e.target.value);
+  };
+
+  const newAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const addBook = (e) => {
+    e.preventDefault();
+    setBooks((prevBooks) => [...prevBooks, { name: name, author: author }]);
+    setName("");
+    setAuthor("");
+  };
+
+  const deleteBook = (book) => {
+    const existBook = books.find((x) => x.name === book.name);
+    if (existBook) {
+      setBooks(books.filter((x) => x.name !== book.name));
+    }
+  };
+
+  const handleEditName = (e) => {
+    setEditName(e.target.value);
+  };
+
+  const handleEditAuthor = (e) => {
+    setEditAuthor(e.target.value);
+  };
+
+  const handleEditBook = (book) => {
+    const bookId = book.id;
+    console.log(bookId, book.id);
+    setBooks(
+      books.map((book) =>
+        book.id === bookId
+          ? { name: editName, author: editAuthor, id: bookId }
+          : book
+      )
+    );
+    setEditName("");
+    setEditAuthor("");
+  };
+
+  const value = {
+    books,
+    newName,
+    newAuthor,
+    addBook,
+    name,
+    author,
+    editName,
+    editAuthor,
+    deleteBook,
+    handleEditName,
+    handleEditAuthor,
+    handleEditBook,
+  };
+
   return (
-    <BooksContext.Provider value={[books, setBooks]}>
+    <BooksContext.Provider value={value}>
       {props.children}
     </BooksContext.Provider>
   );
