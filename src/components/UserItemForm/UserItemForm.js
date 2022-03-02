fb38315043ed23f0aea296a311622f8d633d9543
Marko-batman <krznarm26@gmail.com>
2022-03-02 21:00:49 +0100

    Added new component only for add new user form & style for that component

---
+++ src/components/UserItemForm/UserItemForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useUsers } from "../../data/UsersContext";
+import { format } from "date-fns";
+
+export default function UserItemForm() {
+  const {
+    newUserName,
+    newUserSurname,
+    newUserBirth,
+    handleNewName,
+    handleNewSurname,
+    handleBirth,
+    handleSubmit,
+  } = useUsers();
+  return (
+    <div className="d-flex justify-content-between align-items-center block--form-add">
+      <p>Dodaj novog korisnika</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newUserName}
+          onChange={handleNewName}
+          placeholder="Ime"
+        />
+        <input
+          type="text"
+          value={newUserSurname}
+          onChange={handleNewSurname}
+          placeholder="Prezime"
+        />
+        <input
+          type="date"
+          id="start"
+          name="birth"
+          value={newUserBirth}
+          max={format(new Date(), "yyyy-MM-dd")}
+          onChange={handleBirth}
+        ></input>
+        <button>Dodaj</button>
+      </form>
+    </div>
+  );
+}
