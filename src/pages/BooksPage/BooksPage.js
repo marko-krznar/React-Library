45e91627c29edc656d3885b0773b5b1389b0ce35
Marko-batman <krznarm26@gmail.com>
2022-03-05 12:52:52 +0100

    Created style file and imported in component, added style for smaller screen sizes for add form block and table block

--- src/pages/BooksPage/BooksPage.js
+++ src/pages/BooksPage/BooksPage.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import BookItem from "../../components/BookItem/BookItem";
 import BookItemForm from "../../components/BookItemForm/BookItemForm";
 import { useBooks } from "../../contexts/BooksContext";
+
+import "./style.scss";
 
 export default function BooksPage() {
   const { books } = useBooks();
@@ -9,19 +12,21 @@ export default function BooksPage() {
     <section className="page pg-books">
       <h2 className="headline">Knjige</h2>
       <BookItemForm />
-      <table>
-        <thead>
-          <tr>
-            <th colSpan={1}>Naziv knjige</th>
-            <th colSpan={3}>Autor knjige</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.map((book, index) => (
-            <BookItem key={index} book={book} />
-          ))}
-        </tbody>
-      </table>
+      <div className="block--table">
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={1}>Naziv knjige</th>
+              <th colSpan={3}>Autor knjige</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => (
+              <BookItem key={index} book={book} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
